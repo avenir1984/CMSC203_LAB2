8b06295ff427c97a8d9c34ca09d490108588c10d
avenir1984 <60416229+avenir1984@users.noreply.github.com>
2020-09-13 21:08:15 -0400

    Add files via upload

---
+++ WiFiDiagnosisTest.java
@@ -0,0 +1,12 @@
+
+public class WiFiDiagnosisTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("If you have a problem with internet connectivity,"
+				+ " this WiFi Diagnosis might work.\n");
+		WiFiDiagnosis diagnosis = new WiFiDiagnosis();
+		diagnosis.resetWiFi();
+	}
+
+}
